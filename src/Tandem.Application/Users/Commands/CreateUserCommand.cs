8e5c73150fa99f4c09d76b3f105350e431bed2e3
David Palumbo <david.palumbo@hotmail.com>
2020-07-29 19:35:14 -0400

    Add CreateUserCommand.

---
+++ src/Tandem.Application/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using MediatR;
+
+namespace Tandem.Application.Users.Commands
+{
+    /// <summary>
+    /// Command data for creating a new user.
+    /// </summary>
+    public class CreateUserCommand : IRequest<Guid>
+    {
+        /// <summary>
+		/// Gets or sets the first name of the user.
+		/// </summary>
+		public string FirstName { get; set; }
+
+        /// <summary>
+		/// Gets or sets the middle name for the user.
+		/// </summary>
+		public string MiddleName { get; set; }
+
+        /// <summary>
+		/// Gets or sets the last name for the user.
+		/// </summary>
+		public string LastName { get; set; }
+
+        /// <summary>
+		/// Gets or sets the phone number for the user.
+		/// </summary>
+		public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address for the user.
+        /// </summary>
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ src/Tandem.Domain/Users/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Tandem.Domain.Users
+{
+    /// <summary>
+    /// Interface for types that will provide persistent storage access for users.
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Saves the supplied <paramref name="user"/> to persistent storage.
+        /// </summary>
+        /// <param name="user">
+        /// Required user to save.
+        /// </param>
+        Task SaveUserAsync(User user);
+    }
+}

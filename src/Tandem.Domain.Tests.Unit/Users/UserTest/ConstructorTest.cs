75e5f30aea7bc126713975c95e59c2ef34b0add9
David Palumbo <david.palumbo@hotmail.com>
2020-07-29 18:54:00 -0400

    Add user class.

---
+++ src/Tandem.Domain.Tests.Unit/Users/UserTest/ConstructorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tandem.Domain.Users;
+
+namespace Tandem.Domain.Tests.Unit.Users.UserTest
+{
+    /// <summary>
+    /// Unit tests for the constructors of the <see cref="User"/> class.
+    /// </summary>
+    [TestClass]
+    public class ConstructorTest
+    {
+        /// <summary>
+        /// Tests that a new instance of the <see cref="User"/> class has the
+        /// expected state.
+        /// </summary>
+        [TestMethod]
+        public void NewInstanceShouldHaveExpectedState()
+        {
+            User user = new User();
+
+            Assert.AreNotEqual(Guid.Empty, user.Id);
+        }
+    }
+}
---
+++ src/Tandem.Domain/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tandem.Domain.Users
+{
+    /// <summary>
+    /// Represents a user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="User"/> class.
+        /// </summary>
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Gets the unique identifier for this user
+        /// </summary>
+        public Guid Id { get; }
+    }
+}

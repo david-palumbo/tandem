18f5fd4bd6282ffa1586b39129b18c65c2f77503
David Palumbo <david.palumbo@hotmail.com>
2020-07-29 22:02:26 -0400

    Add mapping from create user command to user entity.

---
+++ src/Tandem.Application.Tests.Unit/Users/Commands/CreateUserCommandDomainExtensionsTests/ToUserEntityTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tandem.Application.Users.Commands;
+using Tandem.Domain.Users;
+
+namespace Tandem.Application.Tests.Unit.Users.Commands.CreateUserCommandDomainExtensionsTests
+{
+    /// <summary>
+    /// Unit tests for the ToUserEntity method of the
+    /// <see cref="CreateUserCommandDomainExtensions"/> class
+    /// </summary>
+    [TestClass]
+    public class ToUserEntityTest
+    {
+        /// <summary>
+        /// Tests that the data in the command is properly mapped to the properties
+        /// of the returned <see cref="User"/> entity.
+        /// </summary>
+        [TestMethod]
+        public void ShouldMapCommandDataToEntityProperties()
+        {
+            CreateUserCommand command = new CreateUserCommand()
+            {
+                FirstName = "Some",
+                MiddleName = "Kindof",
+                LastName = "User",
+                EmailAddress = "some@user.foo",
+                PhoneNumber = "999-999-9999"
+            };
+
+            User user = command.ToUserEntity();
+
+            Assert.AreEqual(command.FirstName, user.Name.First);
+            Assert.AreEqual(command.MiddleName, user.Name.Middle);
+            Assert.AreEqual(command.LastName, user.Name.Last);
+            Assert.AreEqual(command.PhoneNumber, user.PhoneNumber.Value);
+            Assert.AreEqual(command.EmailAddress, user.EmailAddress.Value);
+        }
+    }
+}
---
+++ src/Tandem.Application/Users/Commands/CreateUserCommandDomainExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Tandem.Domain.Users;
+
+namespace Tandem.Application.Users.Commands
+{
+    /// <summary>
+    /// Extends the <see cref="CreateUserCommand"/> class to integrate with the
+    /// domain objects.
+    /// </summary>
+    public static class CreateUserCommandDomainExtensions
+    {
+        /// <summary>
+        /// Converts the <paramref name="command"/> to a User entity.
+        /// </summary>
+        /// <param name="command">
+        /// Required command to convert.
+        /// </param>
+        public static User ToUserEntity(this CreateUserCommand command)
+        {
+            PersonName name = new PersonName(
+                command.FirstName, 
+                command.MiddleName, 
+                command.LastName);
+
+            PhoneNumber phoneNumber = new PhoneNumber(command.PhoneNumber);
+            EmailAddress emailAddress = new EmailAddress(command.EmailAddress);
+
+            return new User(name, phoneNumber, emailAddress);
+        }
+    }
+}

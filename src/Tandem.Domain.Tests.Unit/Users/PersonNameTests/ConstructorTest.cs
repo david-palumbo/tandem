6f07f94855fcd282d53d8966f200b842d01b39c8
David Palumbo <david.palumbo@hotmail.com>
2020-07-29 21:14:32 -0400

    Add person name type.

---
+++ src/Tandem.Domain.Tests.Unit/Users/PersonNameTests/ConstructorTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tandem.Domain.Users;
+using Tandem.Kernel;
+
+namespace Tandem.Domain.Tests.Unit.Users.PersonNameTests
+{
+    /// <summary>
+    /// Unit tests for the constructors of the <see cref="PersonName"/> class.
+    /// </summary>
+    [TestClass]
+    public class ConstructorTest
+    {
+        /// <summary>
+        /// Tests that passing a null firstName argument will result
+        /// in an <see cref="ValidationException" /> being thrown.
+        /// </summary>
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        public void NullFirstNameShouldThrowException()
+        {
+            ValidationException exception = Assert.ThrowsException<ValidationException>(
+                () => new PersonName(null, "Other", "User"));
+            Assert.AreEqual<string>("First name is required.", exception.UserFriendlyMessage);
+        }
+
+        /// <summary>
+        /// Tests that passing a null lastName argument will result
+        /// in an <see cref="ValidationException" /> being thrown.
+        /// </summary>
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        public void NullLastShouldThrowException()
+        {
+            ValidationException exception = Assert.ThrowsException<ValidationException>(
+                () => new PersonName("Some", "Other", null));
+            Assert.AreEqual<string>("Last name is required.", exception.UserFriendlyMessage);
+        }
+
+        /// <summary>
+        /// Tests that the firstName argument binds to the First property.
+        /// </summary>
+        [TestMethod]
+        public void FirstNameArgumentShouldBindToProperty()
+        {
+            const string firstName = "Some";
+
+            PersonName personName = new PersonName(firstName, null, "User");
+
+            Assert.AreEqual(firstName, personName.First);
+        }
+
+        /// <summary>
+        /// Tests that the middleName argument binds to the First property.
+        /// </summary>
+        [TestMethod]
+        public void MiddleNameArgumentShouldBindToProperty()
+        {
+            const string middleName = "Other";
+
+            PersonName personName = new PersonName("Some", middleName, "User");
+
+            Assert.AreEqual(middleName, personName.Middle);
+        }
+
+        /// <summary>
+        /// Tests that the lastName argument binds to the First property.
+        /// </summary>
+        [TestMethod]
+        public void LastNameArgumentShouldBindToProperty()
+        {
+            const string lastName = "User";
+
+            PersonName personName = new PersonName("Some", null, lastName);
+
+            Assert.AreEqual(lastName, personName.Last);
+        }
+    }
+}
---
+++ src/Tandem.Domain/Users/PersonName.cs
@@ -0,0 +1,60 @@
+﻿using Tandem.Kernel;
+
+namespace Tandem.Domain.Users
+{
+    /// <summary>
+    /// Represents the name of a person.
+    /// </summary>
+    public class PersonName
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="PersonName"/> class.
+        /// </summary>
+        /// <param name="firstName">
+        /// Required firstName name for the person.
+        /// </param>
+        /// <param name="middleName">
+        /// Optional middleName name for the person.
+        /// </param>
+        /// <param name="lastName">
+        /// Required lastName name for the person.
+        /// </param>
+        public PersonName(string firstName, string middleName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ValidationException()
+                {
+                    UserFriendlyMessage = "First name is required."
+                };
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ValidationException()
+                {
+                    UserFriendlyMessage = "Last name is required."
+                };
+            }
+
+            First = firstName;
+            Middle = middleName;
+            Last = lastName;
+        }
+
+        /// <summary>
+		/// Gets the firstName name.
+		/// </summary>
+		public string First { get; }
+
+        /// <summary>
+        /// Gets the middleName name.
+        /// </summary>
+        public string Middle { get; }
+
+        /// <summary>
+        /// Gets the lastName name.
+        /// </summary>
+        public string Last { get; }
+    }
+}

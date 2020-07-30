de111949e562d71067a851e8b329f7d0fe333547
David Palumbo <david.palumbo@hotmail.com>
2020-07-29 21:34:02 -0400

    Add properties to user entity.

--- src/Tandem.Domain.Tests.Unit/Users/UserTest/ConstructorTest.cs
+++ src/Tandem.Domain.Tests.Unit/Users/UserTest/ConstructorTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -19,9 +20,57 @@ namespace Tandem.Domain.Tests.Unit.Users.UserTest
         [TestMethod]
         public void NewInstanceShouldHaveExpectedState()
         {
-            User user = new User();
+            User user = new User(validPersonName, validPhoneNumber, validEmailAddress);
 
             Assert.AreNotEqual(Guid.Empty, user.Id);
+            Assert.AreEqual(validPersonName, user.Name);
+            Assert.AreEqual(validPhoneNumber, user.PhoneNumber);
+            Assert.AreEqual(validEmailAddress, user.EmailAddress);
         }
+
+        /// <summary>
+        /// Tests that passing a null name argument will result
+        /// in an <see cref="ArgumentNullException" /> being thrown.
+        /// </summary>
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        public void NullNameShouldThrowException()
+        {
+            ArgumentNullException exception = Assert.ThrowsException<ArgumentNullException>(
+                () => new User(null, validPhoneNumber, validEmailAddress));
+            Assert.AreEqual<string>("name", exception.ParamName);
+        }
+
+        /// <summary>
+        /// Tests that passing a null phoneNumber argument will result
+        /// in an <see cref="ArgumentNullException" /> being thrown.
+        /// </summary>
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        public void NullPhoneNumberShouldThrowException()
+        {
+            ArgumentNullException exception = Assert.ThrowsException<ArgumentNullException>(
+                () => new User(validPersonName, null, validEmailAddress));
+            Assert.AreEqual<string>("phoneNumber", exception.ParamName);
+        }
+
+        /// <summary>
+        /// Tests that passing a null emailAddress argument will result
+        /// in an <see cref="ArgumentNullException" /> being thrown.
+        /// </summary>
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        public void NullEmailAddressShouldThrowException()
+        {
+            ArgumentNullException exception = Assert.ThrowsException<ArgumentNullException>(
+                () => new User(validPersonName, validPhoneNumber, null));
+            Assert.AreEqual<string>("emailAddress", exception.ParamName);
+        }
+
+        private readonly PersonName validPersonName = new PersonName("Some", null, "User");
+
+        private readonly PhoneNumber validPhoneNumber = new PhoneNumber("777-888-9999");
+
+        private readonly  EmailAddress validEmailAddress = new EmailAddress("dark-helmet@spaceball.one");
     }
 }
--- src/Tandem.Domain/Users/User.cs
+++ src/Tandem.Domain/Users/User.cs
@@ -10,8 +10,23 @@ namespace Tandem.Domain.Users
         /// <summary>
         /// Creates a new instance of the <see cref="User"/> class.
         /// </summary>
-        public User()
+        /// <param name="name">
+        /// Required name for the user.
+        /// </param>
+        /// <param name="phoneNumber">
+        /// Required phone number for the user.
+        /// </param>
+        /// <param name="emailAddress">
+        /// Required email address for the user.
+        /// </param>
+        public User(PersonName name, PhoneNumber phoneNumber, EmailAddress emailAddress)
         {
+            Name = name 
+                ?? throw new ArgumentNullException(nameof(name));
+            PhoneNumber = phoneNumber 
+                ?? throw new ArgumentNullException(nameof(phoneNumber));
+            EmailAddress = emailAddress 
+                ?? throw new ArgumentNullException(nameof(emailAddress));
             Id = Guid.NewGuid();
         }
 
@@ -19,5 +34,20 @@ namespace Tandem.Domain.Users
         /// Gets the unique identifier for this user
         /// </summary>
         public Guid Id { get; }
+
+        /// <summary>
+        /// Gets the name for this user.
+        /// </summary>
+        public PersonName Name { get; }
+
+        /// <summary>
+        /// Gets the phone number for this user.
+        /// </summary>
+        public PhoneNumber PhoneNumber { get; }
+
+        /// <summary>
+        /// Gets the email address for this user.
+        /// </summary>
+        public EmailAddress EmailAddress { get; }
     }
 }

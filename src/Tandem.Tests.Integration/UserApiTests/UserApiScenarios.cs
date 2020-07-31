12a6174e7da78a06d9167828d612c27cab6968eb
David Palumbo <david.palumbo@hotmail.com>
2020-07-31 00:07:37 -0400

    Add sad path integration tests.

--- src/Tandem.Tests.Integration/UserApiTests/UserApiScenarios.cs
+++ src/Tandem.Tests.Integration/UserApiTests/UserApiScenarios.cs
@@ -40,6 +40,51 @@ namespace Tandem.Tests.Integration.UserApiTests
             Assert.AreEqual(command.EmailAddress, savedUser.EmailAddress);
         }
 
+        /// <summary>
+        /// Tests that searching for a user that does not exists will return a 404.
+        /// </summary>
+        [TestMethod]
+        public async Task SearchForNonExistingUser()
+        {
+            swaggerClient client = new swaggerClient(baseUrl, httpClient);
+
+            ApiException<ProblemDetails> exception
+                = await Assert.ThrowsExceptionAsync<ApiException<ProblemDetails>>(
+                    () => client.Users2Async($"{Guid.NewGuid()}@foo.bar"));
+
+            Assert.AreEqual(404, exception.StatusCode);
+        }
+
+        /// <summary>
+        /// Tests that a 400 response is returned when submitting bad data.
+        /// </summary>
+        [TestMethod]
+        public async Task CreateUserWithBadData()
+        {
+            swaggerClient client = new swaggerClient(baseUrl, httpClient);
+
+            CreateUserCommand command = new CreateUserCommand()
+            {
+                FirstName = "Some",
+                MiddleName = "Tandem",
+                LastName = "User",
+                PhoneNumber = "not a phone number",
+                EmailAddress = $"{Guid.NewGuid()}@foo.bar"
+            };
+
+            ApiException phoneNumberException = await Assert.ThrowsExceptionAsync<ApiException>(
+                () => client.UsersAsync(command));
+
+            command.PhoneNumber = "222-333-4444";
+            command.EmailAddress = "not an email";
+
+            ApiException emailException = await Assert.ThrowsExceptionAsync<ApiException>(
+                () => client.UsersAsync(command));
+
+            Assert.AreEqual(400, phoneNumberException.StatusCode);
+            Assert.AreEqual(400, emailException.StatusCode);
+        }
+
         private static HttpClient httpClient = new HttpClient();
         private const string baseUrl = "https://tandemstuff.azurewebsites.net/";
     }
